b0d8fd15f24dd07d3bc04ef941420b0debec762e
Zarina138 <zrinaabdulazizova@gmail.com>
2025-10-05 19:35:53 +0000

    Task

--- main2.c
+++ main2.c
@@ -1,61 +1,109 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
-/**
- * main - simple UNIX command line interpreter
- *
- * Return: 0 on success
- */
-int main(void)
+int main(int argc, char **argv, char **envp)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+    char *line;
+    size_t len;
+    char *args[100];
+    int line_no;
+    int status;
+    int i;
+    char *token;
+    char *fullpath;
     pid_t pid;
-    char *argv[2];
+    int wstatus;
+
+    (void)argc;
+
+    line = NULL;
+    len = 0;
+    line_no = 0;
+    status = 0;
 
     while (1)
     {
-        /* Prompt yalnız interaktiv terminalda */
+        line_no++;
         if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "$ ", 2);
+            printf("($) ");
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1) /* Ctrl+D */
+        if (getline(&line, &len, stdin) == -1)
         {
-            write(STDOUT_FILENO, "\n", 1);
+            if (isatty(STDIN_FILENO))
+                putchar('\n');
             break;
         }
 
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
+        line[strcspn(line, "\n")] = 0;
+
+        if (line[0] == '\0')
+            continue;
+
+        i = 0;
+        token = strtok(line, " \t");
+        while (token && i < 99)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " \t");
+        }
+        args[i] = NULL;
+
+        if (!args[0])
+            continue;
+
+        
+        if (strcmp(args[0], "exit") == 0)
+        {
+            free(line);
+            return status;
+        }
+
+        
+        if (strcmp(args[0], "env") == 0)
+        {
+            int j = 0;
+            while (envp[j])
+            {
+                printf("%s\n", envp[j]);
+                j++;
+            }
+            continue;
+        }
+
+        fullpath = find_command(args[0], envp);
+        if (!fullpath)
+        {
+            fprintf(stderr, "%s: %d: %s: not found\n", argv[0], line_no, args[0]);
+            status = 127;
+            continue;
+        }
 
         pid = fork();
         if (pid == -1)
         {
-            perror("fork failed");
-            continue;
+            perror("fork");
+            free(fullpath);
+            free(line);
+            exit(1);
         }
 
         if (pid == 0)
         {
-            /* Child process */
-            argv[0] = line;
-            argv[1] = NULL;
-            execve(line, argv, NULL); /* NULL environment if not needed */
-            perror("Error");           /* Executable not found */
-            exit(EXIT_FAILURE);
+            execve(fullpath, args, envp);
+            perror("execve");
+            exit(1);
         }
         else
         {
-            /* Parent process */
-            wait(NULL);
+            waitpid(pid, &wstatus, 0);
+            if (WIFEXITED(wstatus))
+                status = WEXITSTATUS(wstatus);
+            else
+                status = 1;
         }
+
+        free(fullpath);
     }
 
     free(line);
-    return 0;
+    return status;
 }
