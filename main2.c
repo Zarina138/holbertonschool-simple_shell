f26849f00a64ab0d84346bd727696f17418cfe19
Zarina138 <zrinaabdulazizova@gmail.com>
2025-10-05 19:20:59 +0000

    Task

---
+++ main2.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * main - Simple UNIX command line interpreter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+	char *argv[2];
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		nread = getline(&line, &len, stdin);
+
+		if (nread == -1)
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
+
+		pid = fork();
+		if (pid == 0)
+		{
+			argv[0] = line;
+			argv[1] = NULL;
+			execve(line, argv, NULL);
+			perror("Error");
+			exit(1);
+		}
+		else
+			wait(NULL);
+	}
+
+	free(line);
+	return (0);
+}
