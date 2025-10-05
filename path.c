7f7d357ad5bae0f42e9ac696fc97559644c681a7
Zarina138 <zrinaabdulazizova@gmail.com>
2025-10-05 19:33:44 +0000

    Task

---
+++ path.c
@@ -0,0 +1,64 @@
+#include "shell.h"
+
+char *find_command(char *command, char **envp)
+{
+    char *path_env = NULL;
+    char *path_copy, *dir;
+    char *fullpath;
+    int i;
+
+    for (i = 0; envp[i]; i++)
+    {
+        if (strncmp(envp[i], "PATH=", 5) == 0)
+        {
+            path_env = envp[i] + 5;
+            break;
+        }
+    }
+
+    if (!path_env || strchr(command, '/'))
+    {
+        fullpath = strdup(command);
+        if (!fullpath)
+        {
+            perror("strdup");
+            exit(1);
+        }
+        if (access(fullpath, X_OK) == 0)
+            return fullpath;
+        free(fullpath);
+        return NULL;
+    }
+
+    path_copy = strdup(path_env);
+    if (!path_copy)
+    {
+        perror("strdup");
+        exit(1);
+    }
+
+    dir = strtok(path_copy, ":");
+    while (dir)
+    {
+        fullpath = malloc(strlen(dir) + strlen(command) + 2);
+        if (!fullpath)
+        {
+            perror("malloc");
+            free(path_copy);
+            exit(1);
+        }
+        sprintf(fullpath, "%s/%s", dir, command);
+
+        if (access(fullpath, X_OK) == 0)
+        {
+            free(path_copy);
+            return fullpath;
+        }
+
+        free(fullpath);
+        dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return NULL;
+}
