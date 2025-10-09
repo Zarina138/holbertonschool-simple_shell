604640b88a589fbce8f446c92743f2d20a3df922
Zarina138 <zrinaabdulazizova@gmail.com>
2025-10-09 14:20:43 +0000

    lost!

--- helpers.c
+++ helpers.c
@@ -1,44 +1,43 @@
 #include "shell.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 /**
  * find_command_in_path - Searches for a command in the system PATH
  * @command: Command name to search for
  *
  * Return: Full path to the command if found, NULL otherwise
+ * Caller must free the returned string.
  */
 char *find_command_in_path(const char *command)
 {
     char *path, *path_copy, *dir;
-    static char full_path[1024];
     struct stat st;
+    char tmp[1024];
 
-    /* PATH-ı al */
     path = getenv("PATH");
     if (!path)
-        return (NULL);
+        return NULL;
 
-    /* PATH-ın surətini çıxart (strtok üçün) */
     path_copy = strdup(path);
     if (!path_copy)
-        return (NULL);
+        return NULL;
 
-    /* PATH-ı : işarəsinə görə ayır */
     dir = strtok(path_copy, ":");
     while (dir)
     {
-        /* tam path düzəlt: dir + "/" + command */
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
-
-        /* fayl mövcuddur və icraolunandırsa qaytar */
-        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+        snprintf(tmp, sizeof(tmp), "%s/%s", dir, command);
+        if (stat(tmp, &st) == 0 && (st.st_mode & S_IXUSR))
         {
             free(path_copy);
-            return (full_path);
+            return strdup(tmp);
         }
-
         dir = strtok(NULL, ":");
     }
 
     free(path_copy);
-    return (NULL);
+    return NULL;
 }
